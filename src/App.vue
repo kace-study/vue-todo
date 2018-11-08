2e7990b53ed3b3a703a54b547a1ddef3e2e764a6
kace-study <kace.study@gmail.com>
2018-11-08 11:58:33 +0900

    complete template

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,73 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="row">
+      <div class="col s12 m8 offset-m2">
+        <h4 class="title center-align">TO DO LIST</h4>
+        <form @submit.prevent="addTodo">
+          <input type="text" name="text" v-model="input">
+          <p v-if="feedback" class="red-text">{{ feedback }}</p>
+        </form>
+        <a @click="deleteAll" class="waves-effect waves-light btn-small right">DELETE</a>
+        <Todo 
+          :todoItems="todoItems"
+          @todo-toggled="onTodoToggled" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Todo from '@/components/Todo'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Todo
+  },
+  data() {
+    return {
+      todoItems: [],
+      input: null,
+      feedback: null,
+    }
+  },
+  methods: {
+    addTodo() {
+      if (this.input) {
+        // create a new todo item
+        let newTodoItem = {
+          title: this.input,
+          isCompleted: false,
+          id: this.todoItems.length + 1
+        }
+        // add to array
+        this.todoItems.push(newTodoItem)
+        this.input = null
+        this.feedback = null
+      } else {
+        this.feedback = 'Please input a new todo!'
+      }
+    },
+    deleteAll() {
+      this.todoItems.splice(0, this.todoItems.length);
+    },
+    onTodoToggled(newTodoItem) {
+      this.todoItems.forEach(todoItem => {
+        if (todoItem.id === newTodoItem.id) {
+          todoItem.isCompleted =  !todoItem.isCompleted
+        } 
+      })
+    }
+  },
+  created() {
+    this.todoItems = [
+      { title: 'Vue Practice', isCompleted: true, id: '1'},
+      { title: 'Using firebase to deploy application', isCompleted: false, id: '2'},
+      { title: 'Upload codes to github', isCompleted: true, id: '3'}
+    ]
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/Todo.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="todo">
+    <h5>Items</h5>
+    <ul>
+      <p v-for="item in items" :key="item.id" @click.prevent="toggleItem(item)">
+        <label>
+          <input type="checkbox" :checked="item.isCompleted"/>
+          <span>{{ item.title }}</span>
+        </label>
+      </p>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Todo',
+  props: {
+    todoItems: Array
+  },
+  data() {
+    return {
+      items: []
+    }
+  },
+  methods: {
+    toggleItem(item) {
+      this.$emit('todo-toggled', item)
+    }
+  },
+  mounted() {
+    this.items = this.todoItems
+  }
+}
+</script>
+
+<style>
+html, input {
+  font-family: 'Lato', sans-serif;
+}
+</style>
